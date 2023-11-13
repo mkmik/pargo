523fb1ea3ebb3e8a27a7b0796d45f73e87195f16
Marko Mikulicic <mmikulicic@gmail.com>
2023-11-13 23:39:24 +0100

    Refactor command line argument parsing to use the `clap` crate.

--- pargo/src/main.rs
+++ pargo/src/main.rs
@@ -9,6 +9,23 @@ pub enum Error {
 // Type alias for Result with the custom Error type
 type Result<T, E = Error> = std::result::Result<T, E>;
 
+/// This struct defines the command line arguments we accept.
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Opts {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Parser)]
+enum Commands {
+    /// Build an binary
+    Build {},
+
+    /// Build a binary and load the binary in the simulator
+    Run {},
+}
+
 fn main() -> Result<()> {
     println!("Hello, pargo!");
     // Here you can use your custom error type with `?` or return it explicitly
