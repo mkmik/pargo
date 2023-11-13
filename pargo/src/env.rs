340767183c1050d852ffe79b1985c78ce62aabcc
Marko Mikulicic <mmikulicic@gmail.com>
2023-11-14 00:32:27 +0100

    aider: Fix the `join` function in `pargo/src/env.rs` to call `as_ref()` on the `base` parameter and change the return type to `PathBuf`.
    # Aider chat conversation:
    
    USER: I ran this command:
    
     cargo build
    
    And got this output:
    
       Compiling pargo v0.1.0 (/Users/mkm/p/rdp11/pargo)
    error[E0599]: no method named `join` found for type parameter `B` in the current scope
      --> pargo/src/env.rs:17:13
       |
    16 | fn join<B: AsRef<Path>, P: AsRef<Path>>(base: B, path: P) -> std::io::Result<PathBuf> {
       |         - method `join` not found for this type parameter
    17 |     Ok(base.join(path))
       |             ^^^^ method not found in `B`
    
    For more information about this error, try `rustc --explain E0599`.
    error: could not compile `pargo` (lib) due to previous error
    
    ASSISTANT: The error message indicates that the `join` method is not found for the type parameter `B` in the `join` function within `pargo/src/env.rs`. This is likely because the `AsRef<Path>` trait does not guarantee the presence of a `join` method. The `join` method is associated with the `Path` and `PathBuf` types, not the `AsRef<Path>` trait.
    
    To fix this issue, we need to ensure that we are calling `join` on a `Path` or `PathBuf` type. We can do this by converting the `AsRef<Path>` to a `&Path` using the `as_ref` method before calling `join`.
    
    Here is the change that needs to be made in `pargo/src/env.rs`:
    
    ```rust
    pargo/src/env.rs
    <<<<<<< SEARCH
    fn join<B: AsRef<Path>, P: AsRef<Path>>(base: B, path: P) -> std::io::Result<PathBuf> {
        Ok(base.join(path))
    =======
    fn join<B: AsRef<Path>, P: AsRef<Path>>(base: B, path: P) -> PathBuf {
        base.as_ref().join(path)
    >>>>>>> REPLACE
    ```
    
    This change modifies the `join` function to call `as_ref()` on the `base` parameter, which converts it to a `&Path`, and then calls `join` on it. Additionally, the return type of the function is changed from `std::io::Result<PathBuf>` to `PathBuf` since the `join` operation does not produce an I/O error. If the `join` function is used in a context where an I/O error can occur, the calling code should handle that error.

--- pargo/src/env.rs
+++ pargo/src/env.rs
@@ -4,15 +4,21 @@ use crate::conf::Config;
 
 pub struct Env {
     pub config: Config,
-    pub target_dir: PathBuf,
+    target_dir: PathBuf,
 }
 
 impl Env {
     pub fn build_dir(&self) -> std::io::Result<PathBuf> {
-        join(self.target_dir, "build")
+        join(&self.target_dir, "build")
+    }
+
+    pub fn target_dir(&self) -> std::io::Result<PathBuf> {
+        join(&self.target_dir, "build")
     }
 }
 
 fn join<B: AsRef<Path>, P: AsRef<Path>>(base: B, path: P) -> std::io::Result<PathBuf> {
-    Ok(base.join(path))
+    let res = base.as_ref().join(path);
+    std::fs::create_dir_all(&res)?;
+    Ok(res)
 }
