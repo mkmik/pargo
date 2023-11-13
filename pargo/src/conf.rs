b388cd647a57b217d8f9c6b444c59f262a330240
Marko Mikulicic <mmikulicic@gmail.com>
2023-11-14 00:08:15 +0100

    Add Pdp11 struct to Config and conf.rs.

--- pargo/src/conf.rs
+++ pargo/src/conf.rs
@@ -1,11 +1,14 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     pub program: Program,
+    pub pdp11: Option<Pdp11>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub struct Program {
     pub name: String,
     pub version: String,
@@ -13,7 +16,15 @@ pub struct Program {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub enum Platform {
     #[serde(rename = "pdp11")]
     Pdp11,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct Pdp11 {
+    pub cpu: String,
+    pub mem: String,
 }
