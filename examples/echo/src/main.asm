89a7574b97f06bca758fa7e73b079815ddffd834
Marko Mikulicic <mmikulicic@gmail.com>
2023-11-14 16:01:07 +0100

    Rename ptap to lda

--- examples/echo/src/main.asm
+++ examples/echo/src/main.asm
@@ -6,14 +6,21 @@
 stack = 10000
 ustack = 16000
 
+t_exit = 1 ; my custom trap id: exit user process
+t_max = 1 ; max syscall
+
 .asect
 .= 1000
     mov #stack,sp
-    bis #0140000, @#ps
-    mov #ustack,sp
-
-    mov #input, @#kbv   ; install keyboard ISR
+    mov #input, @#v_tti  ; install keyboard ISR
     bis #100, @#kbs     ; enable keyboard interrupts
+
+    mov #on_cpu_err, @#v_cpu
+    mov #on_ill, @#v_ill
+    mov #on_trap, @#v_trap
+
+    bis #0144000, @#ps ; user mode + register set 1
+    mov #ustack,sp
 
     call main
     halt
@@ -23,14 +30,10 @@ main:
     mov #intro, r0
     call print
 
-1$:
     mov @#ps, r0
     call print_oct
 
-    mov #123, r0
-    call print_oct
-
-
+1$:
     mov #prompt, r0
     call print
 
@@ -43,10 +46,9 @@ main:
 
     br 1$
 2$:
-    mov #bye, r0
-    call print
-
-    rts pc
+    trap #t_exit
+    nop
+    nop
 
 ; prints a null terminated string. Converts \n into \r\n
 print:
@@ -76,9 +78,6 @@ print_oct:
     rts pc
 
 input:
-    ;mov @#ps, r0
-    ;call print_oct
-
     movb @#kbd, r1
     putch r1
     cmp r1, #CR
@@ -105,10 +104,61 @@ read_line:
     mov #inbuf, r0
     rts pc
 
+on_trap:
+    mov (sp), r0
+    add #-2, r0
+    movb (r0), r0
+    cmp r0, #t_max
+    bgt s_bad
+    asl r0
+    call @syscalls(r0)
+    rti
+on_cpu_err:
+    mov #m_cpu_err, r0
+    call print
+    mov @#ERR, r0
+    call print_oct
+    br err_at
+on_ill:
+    mov #m_ill, r0
+    call print
+    br err_at
+err_at:
+    mov #m_at, r0
+    call print
+    mov (sp), r0
+    add #-2, r0
+    call print_oct
+    br fatal
+fatal:
+    halt
+    nop
+
+s_exit:
+    mov #bye, r0
+    call print
+    halt
+    nop
+
+s_bad:
+    mov #m_bad_s, r0
+    call print
+    halt
+    nop
+
+syscalls:
+    .word 0
+    .word s_exit
+
 intro:   .asciz  /This is a dummy unix shell:/<NL>
 prompt:  .asciz  /$ /
 bye:     .asciz  /bye/<NL>
 tick:    .asciz  /./
+
+m_cpu_err: .asciz /E: CPU ERR: /
+m_ill:   .asciz /E: Illegal instruction/
+m_at:    .asciz / at /
+m_bad_s: .asciz /E: bad sycall/
 
 .even
 indone: .word 0 ; non-zero when a line is ready 
